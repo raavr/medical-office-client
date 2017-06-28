1895408dbb1238be6b0bbdf589f559790a52d2a7
raavr <rafal.r26@gmail.com>
2017-06-28 23:00:37 +0200

    cleaning up unused controller in account component

--- src/app/account/account.component.js
+++ src/app/account/account.component.js
@@ -1,14 +1,9 @@
 import "./account.component.scss";
 import template from "./account.component.html";
 
-class AccountController {
-
-}
-
 export const AccountComponent = {
     bindings: {
         user: "<"
     },
-    template: template,
-    controller: AccountController
+    template: template
 }
