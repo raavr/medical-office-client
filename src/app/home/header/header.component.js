db6fd11d0473b4b3b27406daab37b535c9a61c1c
raavr <rafal.r26@gmail.com>
2018-07-01 16:47:38 +0200

    modify the carousel imgs and styles

--- src/app/home/header/header.component.js
+++ src/app/home/header/header.component.js
@@ -4,8 +4,8 @@ import template from "./header.component.html";
 class HeaderCtrl {
    $onInit() {
        this.owlItems = [
-           { src: "/assets/images/page-header1.jpg", desc: "Zarejestruj się i umów na wizytę!"},
-           { src: "/assets/images/page-header2.jpg", desc: "Zadbaj o zdrowie swoich zębów!"}
+           { src: "/assets/images/smiles-1.jpg", desc: "Zarejestruj się i umów na wizytę!"},
+           { src: "/assets/images/smiles-2.jpg", desc: "Zadbaj o zdrowie swoich zębów!"}
        ];
    }
 }
